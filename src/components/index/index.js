6a16a561131d2c53ec9692eb0aa65c12d6589ca3
qingqing <alinacici7@gmail.com>
2018-10-30 18:55:58 +0800

    1

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -4,8 +4,8 @@ import { Qs } from '../../tools'
 
 const params = Qs();
 const merchant = params.merchantNumber || params.merchant || 'cjxjx';
-let interval = null;
-let tagCount = 1;
+// let interval = null;
+// let tagCount = 1;
 
 export default class Index extends Component {
   constructor(props) {
@@ -35,9 +35,8 @@ export default class Index extends Component {
   render () {
     return (
       <div className="box">
-        <h2>公司介绍</h2>
-        <p>匠心有品隶属安徽普靖金融服务外包有限公司，致力于为年轻人服务，提供海量优质商品。</p>
-        <button onClick={() => this.setState({value:'x'})}>{this.state.value}</button>
+        <h2>第一个页面</h2>
+        <p>个人经历</p>
       </div>
     )
   }
